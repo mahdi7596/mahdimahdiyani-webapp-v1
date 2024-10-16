8cb151f4a0b4397e666a010f32112025abb80021
mahdi <andalibmahdi45@gmail.com>
2024-10-16 21:19:08 +0330

    custom font added from styles folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,13 @@ function App() {
   return (
     <>
       <p>ایران</p>
+      <p className="font-thin text-6xl">مهدی</p>
+      <p className="font-light text-6xl">مهدی</p>
+      <p className="font-black text-6xl">مهدی</p>
+      <p className="font-semibold text-6xl">مهدی</p>
+      <p className="font-bold text-6xl">مهدی</p>
+      <p className="font-extrablack text-6xl">مهدی</p>
+      <p className="font-heavyBlack text-6xl">مهدی</p>
       <button className="btn btn-warning">مهدی</button>
     </>
   );
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,22 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontWeight: {
+      thin: "100", // Thin (100)
+      extralight: "200", // Extra Light (200)
+      light: "300", // Light (300)
+      normal: "400", // Regular (400)
+      medium: "500", // Medium (500)
+      semibold: "600", // SemiBold (600)
+      bold: "700", // Bold (700)
+      extrabold: "800", // ExtraBold (800)
+      black: "900", // Black (900)
+      extrablack: "950", // Extra Black (950)
+      heavyBlack: "1000", // Ultra Black (1000)
+    },
+    fontFamily: {
+      primaryFont: ["primaryFont"],
+    },
   },
   plugins: [require("daisyui")],
 };
