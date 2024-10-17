125ab95bbe90aa25cb5422068422a59b91a81a9b
mahdi <andalibmahdi45@gmail.com>
2024-10-17 12:10:30 +0330

    custom font colors added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,5 @@
 function App() {
-  return (
-    <>
-      <p className="font-thin text-6xl">مهدی</p>
-      <p className="font-heavyBlack text-6xl">ایران</p>
-      <p className="font-black font-headingFont text-6xl">ایران</p>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,24 +1,60 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+// ? یک وقت هایی که مدیا کوری کاستوم اضافه میکنیم قبلی ها از کار میوفتن و برای رفع این مشکل این رو مینویسم
+// const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    // screens: {
-    //   'xxs': '340px',
-    //   'xs': '440px',
-    //   'xsm': '540px',
-    //   ...defaultTheme.screens,
-    // },
+    fontFamily: {
+      primaryFont: ["primaryFont"],
+      headingFont: ["headingFont"],
+    },
     extend: {
+      // screens: {
+      //   'xxs': '340px',
+      //   'xs': '440px',
+      //   'xsm': '540px',
+      //   ...defaultTheme.screens,
+      // },
+      colors: {
+        primary: "var(--primary)",
+        primary900: "var(--primary-900)",
+        primary800: "var(--primary-800)",
+        primary700: "var(--primary-700)",
+        primary600: "var(--primary-600)",
+        primary500: "var(--primary-500)",
+        primary400: "var(--primary-400)",
+        primary300: "var(--primary-300)",
+        primary200: "var(--primary-200)",
+        primary100: "var(--primary-100)",
+        primary0: "var(--primary-0)",
+
+        neutrals: "var(--neutrals)",
+        neutrals900: "var(--neutrals-900)",
+        neutrals800: "var(--neutrals-800)",
+        neutrals700: "var(--neutrals-700)",
+        neutrals600: "var(--neutrals-600)",
+        neutrals500: "var(--neutrals-500)",
+        neutrals400: "var(--neutrals-400)",
+        neutrals300: "var(--neutrals-300)",
+        neutrals200: "var(--neutrals-200)",
+        neutrals100: "var(--neutrals-100)",
+        neutrals0: "var(--neutrals-0)",
+
+        surfaceBg: "var(--surfaceBg)",
+        surfaceBorder: "var(--surfaceBorder)",
+
+        backgroundColor: "var(--backgroundColor)",
+
+        info: "var(--info)",
+        success: "var(--success)",
+        warn: "var(--warn)",
+        danger: "var(--danger)",
+        teal: "var(--teal)",
+      },
       fontWeight: {
         extrablack: "950", // Extra Black (950)
         heavyBlack: "1000", // Ultra Black (1000)
-        ...defaultTheme.fontWeight,
-      },
-      fontFamily: {
-        primaryFont: ["primaryFont"],
-        headingFont: ["headingFont"],
       },
     },
   },
