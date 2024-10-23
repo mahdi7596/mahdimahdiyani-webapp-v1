a838738a1285c9c40aa7a4660b9537be7273eff8
mahdi <andalibmahdi45@gmail.com>
2024-10-23 12:25:59 +0330

    custom font icon added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,5 @@
 function App() {
-  return (
-    <>
-      <p>mahdi</p>
-      <button>
-        <i className="maicon-share text-5xl"></i>
-      </button>
-      <p>mahdi</p>
-      <button>
-        <i className="maicon-hamburgermenu text-5xl"></i>
-      </button>
-      <span>
-        <i className="maicon-arrrow text-5xl"></i>
-      </span>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
