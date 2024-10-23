e4c7eaaec16ab3a4d00b85ea2f587f3d6e2c1179
mahdi <andalibmahdi45@gmail.com>
2024-10-23 12:34:07 +0330

    update 7 files

--- client/src/components/ui/TextInput.tsx
+++ client/src/components/ui/TextInput.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const TextInput = () => {
   return <div>TextInput</div>;
 };
--- client/src/pages/Aboutus.tsx
+++ client/src/pages/Aboutus.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Aboutus = () => {
   return <div>Aboutus</div>;
 };
