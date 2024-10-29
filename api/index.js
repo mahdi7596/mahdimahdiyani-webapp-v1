ab2b504d796685a116b9d6236c82ca3cea0c09cc
mahdi <andalibmahdi45@gmail.com>
2024-10-29 16:36:30 +0330

    connected db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+// require("dotenv").config();
+const express = require("express");
 const app = express();
+
+mongoose
+  //   .connect("mongodb://localhost/mahdimahdiyani-webapp")
+  // !have to replace this
+  .connect(process.env.MONGO)
+  .then(() => console.log("connected to MongoDB..."))
+  .catch((err) => console.log("could not connect to Mongo Db..", err));
 
 app.listen(3000, () => {
   console.log("server is running on prot 3001");
