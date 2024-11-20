1a65cb2646955b28bd54e7bcfd448e65548bc1fc
mahdi <andalibmahdi45@gmail.com>
2024-11-20 12:37:57 +0330

    update DashPosts.tsx and DashUpdatePost.tsx

--- client/src/components/admin/DashPosts.tsx
+++ client/src/components/admin/DashPosts.tsx
@@ -145,7 +145,22 @@ const DashPosts = () => {
           </table>
         </div>
       ) : (
-        <p>هنوز مقاله ای اضافه نکردید</p>
+        <div role="alert" className="alert bg-info text-primary-content">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="h-6 w-6 shrink-0 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            ></path>
+          </svg>
+          <span>هنوز مقاله ای اضافه نکردید</span>
+        </div>
       )}
       {/* delete modal */}
       <dialog ref={deleteModalRef} id="my_modal_1" className="modal">
