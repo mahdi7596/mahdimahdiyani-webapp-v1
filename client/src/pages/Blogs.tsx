12144a266006e3c8aff60b746807217e21a411d3
mahdi <andalibmahdi45@gmail.com>
2024-11-21 13:57:10 +0330

    update Blogs.tsx

--- client/src/pages/Blogs.tsx
+++ client/src/pages/Blogs.tsx
@@ -1,5 +1,18 @@
+import banner from "../assets/images/banner.jpg";
+
 const Blogs = () => {
-  return <div>Blogs</div>;
+  return (
+    <div className="section-container section-inner-space">
+      <div className="flex bg-blue-500 p-1">
+        <img
+          src={banner}
+          className="w-full h-48 rounded-lg"
+          alt="blogs-list-banner"
+        />
+        <div className="absolute bg-red-500" />
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
