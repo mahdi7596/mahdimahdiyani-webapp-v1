eae4a00bf5afd3dc3ddf9ce6fa7242710e05f55f
mahdi <andalibmahdi45@gmail.com>
2025-02-28 15:21:41 +0330

    update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import Hero from "../components/landing/Hero";
 import LogoTicker from "../components/landing/LogoTicker";
 import Pricing from "../components/landing/Pricing";
 import ProductShowcase from "../components/landing/ProductShowcase";
+import Testimonials from "../components/landing/Testimonials";
 
 const Home = () => {
   return (
@@ -11,7 +12,7 @@ const Home = () => {
       <LogoTicker />
       <ProductShowcase />
       <Pricing />
-      {/* <Testimonials /> */}
+      <Testimonials />
       {/* <CallToAction /> */}
       {/* <Footer /> */}
     </>
