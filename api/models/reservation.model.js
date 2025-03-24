ddef6905bd2caeb1353a49843fca909ef6c05384
mahdi <andalibmahdi45@gmail.com>
2025-03-24 14:03:22 +0330

    refactored reservation shcema

--- api/models/reservation.model.js
+++ api/models/reservation.model.js
@@ -1,44 +1,19 @@
 import mongoose from "mongoose";
 
-const reservationSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    includedServices: {
-      type: [String], // List of services offered
-      required: true,
-    },
-    price: {
-      type: Number,
-      required: true,
-    },
-    date: {
-      type: String, // Storing date as string (e.g., "2025-03-26")
-      required: true,
-    },
-    timeSlot: {
-      type: String, // Storing time range (e.g., "13:00-15:00")
-      required: true,
-    },
-    userId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    status: {
-      type: String,
-      enum: ["pending", "confirmed", "cancelled"],
-      default: "pending",
-    },
+const ReservationSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  reservationTypeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "ReservationType",
+    required: true,
   },
-  { timestamps: true }
-);
+  date: { type: String, required: true }, // Selected date
+  timeSlot: { type: String, required: true }, // Selected time slot
+  status: {
+    type: String,
+    enum: ["pending", "confirmed", "cancelled"],
+    default: "pending",
+  },
+});
 
-const Reservation = mongoose.model("Reservation", reservationSchema);
-export default Reservation;
+export default mongoose.model("Reservation", ReservationSchema);
