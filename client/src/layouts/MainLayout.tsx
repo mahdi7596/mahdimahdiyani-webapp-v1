8585e899fbb1103edd92fed93004ed2300b246bd
mahdi <andalibmahdi45@gmail.com>
2025-04-30 17:39:46 +0330

    added framer motion animation to all the pages so they dont get loaded very fast and we have a better ux #892

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -3,19 +3,22 @@ import { useEffect } from "react";
 import { useLocation, Outlet } from "react-router-dom";
 import Navbar from "../components/header/Navbar";
 import Footer from "../components/Footer";
+import PageWrapper from "../components/PageWrapper";
 
 const MainLayout = () => {
-  const { pathname } = useLocation();
+  const location = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname]);
+  }, [location.pathname]);
 
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-grow">
-        <Outlet />
+        <PageWrapper key={location.pathname}>
+          <Outlet />
+        </PageWrapper>
       </main>
       <Footer />
     </div>
