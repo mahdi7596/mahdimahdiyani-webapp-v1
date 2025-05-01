36916eeff5f13fb753ada821f54413557eaa69b6
mahdi <andalibmahdi45@gmail.com>
2025-05-01 15:10:05 +0330

    refactored config file in order to handle production mode

--- api/config/config.js
+++ api/config/config.js
@@ -1,8 +1,14 @@
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 import dotenv from "dotenv";
 
-dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// validation to check for essential env variables
+// Load .env from parent directory
+dotenv.config({ path: resolve(__dirname, "../.env") });
+
+// validation
 if (!process.env.DB_URL || !process.env.PORT) {
   throw new Error("Missing essential environment variables.");
 }
@@ -13,6 +19,8 @@ const config = {
   jwtSecret: process.env.JWT_SECRET || "default_secret",
   apiUrl: process.env.API_URL,
   apiKey: process.env.API_KEY,
+  frontendUrl: process.env.FRONTEND_URL,
+  backendUrl: process.env.BACKEND_URL,
 };
 
 export default config;
