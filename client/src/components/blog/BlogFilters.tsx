01ad4579d6ded4d21e37606877a7377e615c9a73
mahdi <andalibmahdi45@gmail.com>
2025-05-03 19:14:56 +0330

    added search box component in blog filters #888

--- client/src/components/blog/BlogFilters.tsx
+++ client/src/components/blog/BlogFilters.tsx
@@ -1,5 +1,14 @@
+import Search from "../shared/Search";
+
 const BlogFilters = () => {
-  return <div>BlogFilters</div>;
+  return (
+    <form
+      // onSubmit={handleSubmit}
+      className="flex flex-wrap sm:flex-nowrap items-center gap-y-3 gap-x-3"
+    >
+      <Search className="w-full" />
+    </form>
+  );
 };
 
 export default BlogFilters;
